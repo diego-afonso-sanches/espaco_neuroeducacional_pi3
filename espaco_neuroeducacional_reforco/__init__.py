4859ea068a17766cf354b50f79126db373e5348d
diego-afonso-sanches <diegoafonsodasilva@gmail.com>
2026-04-27 21:50:52 -0300

    Atualização do __init__.py

--- espaco_neuroeducacional_reforco/__init__.py
+++ espaco_neuroeducacional_reforco/__init__.py
@@ -1,4 +1,15 @@
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contato.db'
+database = SQLAlchemy(app)
 
+bootstrap = Bootstrap5(app)
+
+from flask_bcrypt import Bcrypt
 from espaco_neuroeducacional_reforco import routes
+
+app.config['SECRET_KEY'] = '2e54a9fb34553a8831fdbdf814771987'
+
+bcrypt = Bcrypt(app)
